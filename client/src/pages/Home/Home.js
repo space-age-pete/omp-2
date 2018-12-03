c2a65a5a20c0c9bef5e4728fe71019ccf236974c
s-a-p <josh.furlin@gmail.com>
2018-12-03 14:15:48 -0600

    added a bunch of boilerplate type stuff

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import "./Home.css"
+
+class Home extends Component {
+  state = {}
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1> Welcome to OMP </h1>
+        <h3> whatever </h3>
+      </div>
+    )
+  }
+}
+
+export default Home
